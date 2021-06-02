70c988d8480c4f481e0c6e746ec3775027bf6122
jackiemarsh <your_email_herejackie.m.marsh@gmail.com>
2021-06-02 07:51:45 -1000

    Modify reducers structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
-    # resource :events, only: [:create, :destroy]
+    # resource :events, only: [:create, :edit, :show, :destroy]
   end
   root to: "static_pages#root"
 end
