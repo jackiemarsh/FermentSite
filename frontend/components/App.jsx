a3e08ae8265e35b58580d3b3c188446be36deeb9
jackiemarsh <jackie.m.marsh@gmail.com>
2021-06-01 20:00:21 -1000

    Add gretting and container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,10 @@
-
 import React from "react";
+import GreetingContainer from './greeting/greeting_container'
 
 const App = () => (
   <div>
     <h1>FermentSite is brewing</h1>
+    <GreetingContainer />
   </div>
 );
 
