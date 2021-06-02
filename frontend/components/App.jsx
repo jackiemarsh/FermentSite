e35c5fcb2ad4a1426a458e5a46562cb41908b444
jackiemarsh <jackie.m.marsh@gmail.com>
2021-06-02 07:51:45 -1000

    Modify reducers structure

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router";
 import GreetingContainer from './greeting/greeting_container'
+import AuthRoute from './util/route_util'
 
 const App = () => (
   <div>
@@ -9,8 +10,8 @@ const App = () => (
         <GreetingContainer />
        </header>
   
-        <Route path='/login' component={LoginFormContainer}/>
-        <Route path='/signup' component={SignupFormContainer}/>
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
   </div>
 );
 
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux"
-
-import usersReducer from './users_reducer';
+import entititiesReducer from './entities_reducer'
+import sessionReducer from './session_reducer'
+import errorsReducer from './errors_reducer'
 
 const rootReducer = combineReducers({
-    users: usersReducer
+    session: sessionReducer,
+    entities: entitiesReducer,
+    errors: errorsReducer
 })
 
 export default rootReducer
