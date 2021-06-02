1914df4a7eaeda8fa35cf9166b2bb56027b32038
jackiemarsh <your_email_herejackie.m.marsh@gmail.com>
2021-06-01 19:42:33 -1000

    Add App component and update entry file

--- frontend/ferment_site.jsx
+++ frontend/ferment_site.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './components/root'
-// import configureStore from './store/store'
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<Root/>, document.getElementById('root'))
+    const store = configureStore()
+
     window.getState = store.getState;
     window.dispatch = store.dispatch;
+
+    ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 })
