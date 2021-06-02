61e843e01f348e1c2c588f479015a06408782d11
jackiemarsh <jackie.m.marsh@gmail.com>
2021-06-02 08:43:57 -1000

    debug syntax errors to render

--- frontend/ferment_site.jsx
+++ frontend/ferment_site.jsx
@@ -4,20 +4,20 @@ import Root from './components/root'
 import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
-    let store;
-        if (window.currentUser) {
-          const preloadedState = {
-            entities: {
-              users: { [window.currentUser.id]: window.currentUser }
-            },
-            session: { id: window.currentUser.id }
-          };
-          store = configureStore(preloadedState);
-          delete window.currentUser;
-        } else {
-          store = configureStore();
-        }
-        
+    let store = configureStore();
+        // if (window.currentUser) {
+        //   const preloadedState = {
+        //     entities: {
+        //       users: { [window.currentUser.id]: window.currentUser }
+        //     },
+        //     session: { id: window.currentUser.id }
+        //   };
+        //   store = configureStore(preloadedState);
+        //   delete window.currentUser;
+        // } else {
+        //   store = configureStore();
+        // }
+
     window.getState = store.getState;
     window.dispatch = store.dispatch;
 
