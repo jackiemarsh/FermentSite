46f5cf893e01247b8bff4df7b79fafafbaf3251d
jackiemarsh <jackie.m.marsh@gmail.com>
2021-06-03 13:54:49 -1000

    Fix refresh bug

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,14 +1,13 @@
 import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER} from '.././actions/session_actions'
 
-const _nullUser = Object.assign({
-    id: null
-});
+const _nullUser = {id: null};
 
-const sessionReducer = (state={}, action) => {
+const sessionReducer = (state= _nullUser, action) => {
     Object.freeze(state);
+
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return { id: action.currentUser.id}
+            return Object.assign({}, { id: action.currentUser.id })
         case LOGOUT_CURRENT_USER:
             return _nullUser
         default:
