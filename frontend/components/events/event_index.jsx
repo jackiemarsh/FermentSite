28e9d4181a4a416f1408c1ed6e4d77b2a2d530a2
jackiemarsh <your_email_herejackie.m.marsh@gmail.com>
2021-06-04 13:38:29 -1000

    Render hero image splash

--- frontend/components/events/event_index.jsx
+++ frontend/components/events/event_index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import EventIndexItem from './event_index_item';
 import { withRouter } from 'react-router-dom';
+// import * as eventImages from '../../../app/assets/images'
+// import mainLogo from '../../../app/assets/images/beer-wall.jpg'
+
 
 class EventIndex extends React.Component {
     constructor(props) {
@@ -21,6 +24,11 @@ class EventIndex extends React.Component {
         return (
           <div className="event-show">
               <h1>Events</h1>
+              <div className="index-header">
+                <h4 className="index-header-top">Drink life to the last drop</h4>
+                <h2 className="index-header-bottom">Beer events near you</h2>
+                <img src={mainLogo} alt="beer wall image" className="hero-img"/>
+              </div>
               <ul>
                 {this.props.events.map(event => (
                   <EventIndexItem key={event.id} event={event} />
--- frontend/components/events/event_index_item.jsx
+++ frontend/components/events/event_index_item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 class EventIndexItem extends React.Component{
   constructor(props){
     super(props);
