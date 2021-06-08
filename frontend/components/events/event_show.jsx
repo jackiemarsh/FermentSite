c4f8810d135a79d043e9f8db741fc9a42dc2f464
jackiemarsh <your_email_herejackie.m.marsh@gmail.com>
2021-06-07 20:29:07 -1000

    create event show page and tweak event index

---
+++ frontend/components/events/event_show.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import EventIndexItem from './event_index_item';
+
+class EventShow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+        };
+    }
+
+    componentDidMount() {
+        this.props.fetchEvent(this.props.match.params.eventId)
+        .then(() => this.setState({ loading: false}));
+
+        this.setState({ loading: false });
+    }
+
+    render() {
+        if (this.state.loading) return null;
+        if (this.props.event === undefined) return null;
+
+        return (
+            <div>Test Show
+                <section className="event-show">
+                    <div className="event-show-top">
+                        <figure>
+                            <img src={this.props.event.imageUrl} alt="event pic" className="event-show-img"/>
+                        </figure>
+                        <div className="top-side">
+                            <h4 className="event-show-date">{this.props.event.start_date}</h4>
+                            <h2 className="event-show-title">{this.props.event.title}</h2>
+                            <h3 className="event-show-author">by {this.props.author_id}</h3>
+                        </div>
+                    </div>
+                    <div className="event-show-bottom">
+                        <h2 className="event-show-title">{this.props.event.title}</h2>
+                        <h1 className="event-show-header">About this event</h1>
+                        <h4 className="event-show-description">{this.props.event.description}</h4>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+export default (EventShow)
--- frontend/components/session/signup_form_container.jsx
+++ frontend/components/session/signup_form_container.jsx
@@ -1,20 +1,22 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { signup } from '../../actions/session_actions';
+import { signup, resetSessionErrors } from '../../actions/session_actions';
 import SessionForm from './session_form_container';
 
 const mapStateToProps = ({ errors }) => {
   return {
     errors: errors.session,
     formType: 'signup',
-    navLink: <Link to="/login" className="link-text">Log In</Link>,
+    // navLink: <Link to="/login" className="link-text">Log In</Link>,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     processForm: (user) => dispatch(signup(user)),
+    navLink: (<Link to="/login" onClick={(e) => { dispatch(resetSessionErrors()) }} className="link-text">Log In</Link>),
+ 
   };
 };
 
