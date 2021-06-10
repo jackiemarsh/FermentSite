2c4afdae6b8450c5c924ef39681096fdea9b562a
jackiemarsh <jackie.m.marsh@gmail.com>
2021-06-09 15:54:03 -1000

    Fix create event bug, add file upload capability

--- frontend/components/events/create_event_form.jsx
+++ frontend/components/events/create_event_form.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 class CreateEventForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = this.props.event;
+        this.state = {
 
+        title: "",
+        description: "",
+        start_date: "",
+        location: "",
+        image: null,
+        imageUrl: null,
+        author_id: this.props.currentUser.id
+        };
+        console.log(this.state)
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleFile = this.handleFile.bind(this)
     }
 
     update(field) {
@@ -16,8 +26,35 @@ class CreateEventForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        const event = Object.assign({}, this.state);
-        this.props.createEvent(event)
+        // const event = Object.assign({}, this.state);
+        // this.props.createEvent(event)
+        const formData = new FormData();
+        formData.append("event[title]", this.state.title);
+        formData.append("event[description]", this.state.description);
+        formData.append("event[location]", this.state.location);
+        formData.append("event[start_date]", this.state.start_date);
+        formData.append("event[author_id]", this.state.author_id)
+        if (this.state.image) {
+            formData.append("event[image]", this.state.image);
+        }
+        this.props.createEvent(formData)
+
+    }
+
+    // handleFile(e) {
+    //     e.preventDefault();
+    //     this.setState({image: e.currentTarget.files[0]});
+    // }
+    
+    handleFile(e){
+        const file = e.currentTarget.files[0];
+        const filereader = new FileReader();
+        filereader.onloadend = () => {
+            this.setState({image: file, imageUrl: filereader.result});
+        };
+        if (file){
+            filereader.readAsDataURL(file);
+        }
     }
 
     render() {
@@ -50,7 +87,10 @@ class CreateEventForm extends React.Component {
                             onChange={this.update('description')}
                             />
                         </label>
-                        
+                        <label className="event-image">Upload an image
+                            <input type="file"
+                            onChange={this.handleFile.bind(this)}/>
+                        </label>
                     </div>
                     <input className="button-submit" type="submit" value="Save" />
                 </form>
